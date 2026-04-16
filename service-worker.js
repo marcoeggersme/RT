7495390c2322121670a739d8e00560c029dc70ff
marcoeggersme <marcoeggers.me@googlemail.com>
2026-04-16 19:12:42 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "reittherapie-auto-" + new Date().getTime();
+const CACHE_NAME = "reittherapie-static-v1";
 
 const urlsToCache = [
   "./",
@@ -9,38 +9,18 @@ const urlsToCache = [
 
 // INSTALL
 self.addEventListener("install", event => {
-  self.skipWaiting(); // sofort aktiv
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// FETCH
+// FETCH (Network first, dann Cache)
 self.addEventListener("fetch", event => {
   event.respondWith(
     fetch(event.request)
-      .then(response => {
-        return response;
+      .then(res => {
+        return res;
       })
-      .catch(() => {
-        return caches.match(event.request);
-      })
-  );
-});
-
-// ACTIVATE
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(k => {
-          if (k !== CACHE_NAME) {
-            return caches.delete(k);
-          }
-        })
-      )
-    )
+      .catch(() => caches.match(event.request))
   );
 });
